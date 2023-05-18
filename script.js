ffad28e57c9053f2131164b57f5fb96d49e253cf
therandomspoon <107148755+therandomspoon@users.noreply.github.com>
2023-05-18 18:13:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ fetch('pokemon.json')
     .then(response => response.json())
     .then(data => {
         const pokemonList = document.getElementById('pokemonList');
-        data.forEach(pokemon => {
-            const listItem = document.createElement('li');
-            listItem.textContent = pokemon.name;
-            pokemonList.appendChild(listItem);
-        });
+        if (Array.isArray(data)) {
+            data.forEach(pokemon => {
+                const listItem = document.createElement('li');
+                listItem.textContent = pokemon.name;
+                pokemonList.appendChild(listItem);
+            });
+        } else {
+            console.error('Error: Invalid JSON format. Data is not an array.');
+        }
     })
     .catch(error => {
         console.error('Error:', error);
